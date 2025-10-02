79b60928fb10958e74f2d8ef1fd93c870bb0d2dc
Ayazalidharejo <Ayazali.xwave@gmail.com>
2025-10-03 00:09:18 +0500

    change

--- src/pages/ResetPasswordPage.tsx
+++ src/pages/ResetPasswordPage.tsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://tour-backend-eight.vercel.app/api';
 
 const ResetPasswordPage = () => {
   const [password, setPassword] = useState('');
