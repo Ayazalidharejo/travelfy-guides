f27a6180d3530612b3d16dd498851b54720f1c3a
Ayazalidharejo <Ayazali.xwave@gmail.com>
2025-10-06 12:37:10 +0500

    change

--- src/pages/admin/AdminPostDashboard.tsx
+++ src/pages/admin/AdminPostDashboard.tsx
@@ -3459,16 +3459,7 @@ const TourBookingForm = () => {
                       />
                     </div>
                     
-                    <div>
-                      <label className="block text-sm font-medium text-gray-700 mb-1">Nearby Attractions</label>
-                      <textarea 
-                        name="nearbyAttractions" 
-                        value={formData.nearbyAttractions} 
-                        onChange={handleInputChange} 
-                        rows="2" 
-                        className="w-full px-3 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-transparent" 
-                      />
-                    </div>
+                   
                   </div>
                 )}
               </div>
