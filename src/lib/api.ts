975be633aa7654d0882459c9bd6bfc7a6dc8d1fb
Ayazalidharejo <Ayazali.xwave@gmail.com>
2025-10-23 11:41:54 +0500

    change

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const API_BASE_URL = 'http://localhost:5000/api';
-const API_BASE_URL = 'https://tour-backend-eight.vercel.app/api'; 
+// const API_BASE_URL = 'https://tour-backend-eight.vercel.app/api'; 
+const API_BASE_URL = 'tour-backend-production-7311.up.railway.app/api'; 
 // Create axios instance
 const api = axios.create({
   baseURL: API_BASE_URL,
