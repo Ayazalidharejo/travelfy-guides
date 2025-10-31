90526784e4913b084bc590e8158aed214a930790
Ayazalidharejo <Ayazali.xwave@gmail.com>
2025-10-31 16:03:07 +0500

    change

--- src/components/Getyourguide.tsx
+++ src/components/Getyourguide.tsx
@@ -58,28 +58,28 @@ const GetYourGuideTours = () => {
       title: 'Mount Fuji: Customizable Private Tour By Car With Pickup',
       imageUrl: 'https://cdn.getyourguide.com/image/format=auto,fit=crop,gravity=auto,quality=60,width=620,height=400,dpr=1/tour_img/21f992460145df176bc2b06292c52352c8c0797a43a64f5ebb74bcc5d36c9136.jpg',
       description: 'Experience the breathtaking beauty of Mount Fuji and the hot springs of Hakone on this private tour from Tokyo.',
-      url: 'https://www.getyourguide.com/tokyo-l193/mount-fuji-hakone-private-tour-from-tokyo-t858944/?partner_id=KARVAAN&utm_medium=online_publisher&placement=content-middle&cmp=karvaan-tours'
+      url: 'https://www.getyourguide.com/tokyo-l193/mount-fuji-customizable-private-tour-by-car-with-pickup-t550155/?preview=SQSJKVCNLDTPKADFCQRC2HW6C4JDM0TD'
     },
     {
       id: '830175',
       title: 'Tokyo: Mt. Fuji & Lake Kawaguchi Private Tour with Transfer',
       imageUrl: 'https://cdn.getyourguide.com/image/format=auto,fit=crop,gravity=auto,quality=60,width=300,height=400,dpr=1/tour_img/a1fe0ca08b46f116c41cf78695679256122398b307eb927222c8e0f659dfc89d.jpg',
       description: 'Explore Kyoto’s temples and the friendly deer of Nara in one unforgettable day trip from Osaka or Kyoto.',
-      url: 'https://www.getyourguide.com/kyoto-l968/kyoto-nara-day-tour-from-osaka-t830175/?partner_id=KARVAAN&utm_medium=online_publisher&placement=content-middle'
+      url: 'https://www.getyourguide.com/tokyo-l193/mount-fuji-lake-kawaguchi-private-tour-with-hotel-pickup-t858944/?preview=CR2M3ED3O2L4GU5JW2R7SB2EOKVZR9XC'
     },
     {
       id: '752981',
       title: 'Nikko Customize Private Tour By Car with Hotel Pickup',
       imageUrl: 'https://cdn.getyourguide.com/image/format=auto,fit=crop,gravity=auto,quality=60,width=620,height=400,dpr=1/tour_img/257eae91546795f05e9ea446d0f1eafa0a3eeaf3cb928f4189dbd7fd1ac8c682.jpg',
       description: 'Enjoy a personalized sightseeing experience in Tokyo with your own private guide and customizable itinerary.',
-      url: 'https://www.getyourguide.com/tokyo-l193/tokyo-full-day-custom-private-tour-t752981/?partner_id=KARVAAN&utm_medium=online_publisher&placement=content-middle'
+      url: 'https://www.getyourguide.com/nikko-l89510/nikko-customize-private-tour-by-car-with-hotel-pickup-t838926/?preview=BXEPREZBDSG7F41KBPPBXE95MPKYLLKY'
     },
     {
       id: '887654',
-      title: 'Tokyo private tour by car with pick up',
+      title: 'Hakone And Mount Fuji Private Tour With Hotel Pick Up',
       imageUrl: 'https://cdn.getyourguide.com/image/format=auto,fit=crop,gravity=auto,quality=60,width=620,height=400,dpr=1/tour_img/e753bb02342599d435a77cbb952015b8016c7f2e29a157da5185f9a8d44840c3.jpg',
       description: 'Walk through the enchanting Arashiyama Bamboo Grove and visit Kyoto’s UNESCO-listed Tenryu-ji Temple.',
-      url: 'https://www.getyourguide.com/kyoto-l968/arashiyama-bamboo-forest-guided-tour-t887654/?partner_id=KARVAAN&utm_medium=online_publisher&placement=content-middle'
+      url: 'https://www.getyourguide.com/fujiyoshida-l157144/hakone-and-mount-fuji-private-tour-with-hotel-pick-up-t823417/?preview=I9YJJTGDPYJ2JP1DOXMSSCX5MRDYSL1R'
     },
   ];
 
